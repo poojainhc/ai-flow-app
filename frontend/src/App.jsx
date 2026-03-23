23a702316b4c183c7fa41dd1e189291d65647adc
Pooja Jadhav <poojakhawle123@gmail.com>
2026-03-23 20:18:41 +0530

    modify UI  of App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
   const [input, setInput] = useState("");
   const [output, setOutput] = useState("");
   const [loading, setLoading] = useState(false);
+  
   const API = "https://ai-flow-app-hvvz.onrender.com";
   
  const centerX = 400; // middle of container
@@ -71,30 +72,34 @@ const gap = 250; // distance between nodes
 };
 
   return (
-     <div className="h-screen bg-gray-50 p-5">
-      <h1 className="text-2xl font-bold mb-4 text-center">
-        AI Flow Builder
-      </h1>
+     <div className="min-h-screen bg-linear-to-br from-[#132246] to-[#020617] text-white">
+     <div className="flex items-center justify-between px-10 py-4 border-b border-gray-800">
+  <h1 className="text-3xl font-bold text-white!">
+    AI Flow Builder
+  </h1>
+  <div className="flex gap-4">
+    <button
+      onClick={runFlow}
+      className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600"
+    >
+      Run Flow
+    </button>
 
-      <div className="flex justify-center gap-4 mb-4">
-        <button
-          onClick={runFlow}
-          className="bg-blue-500 text-white px-4 py-2 rounded-lg"
-        >
-          Run Flow
-        </button>
+    <button
+      onClick={saveData}
+      className="bg-green-500 text-white px-4 py-2 rounded-lg hover:bg-green-600"
+    >
+      Save
+    </button>
+  </div>
 
-        <button
-          onClick={saveData}
-          className="bg-green-500 text-white px-4 py-2 rounded-lg"
-        >
-          Save
-        </button>
-      </div>
+</div>
 
-      <div className="h-500 w-full flex justify-center  bg-white rounded-xl shadow ">
+      <div className="px-10 py-6">
+      <div className="h-125 w-full bg-[#040e3d] rounded-2xl border border-gray-800 shadow-2xl">
         <ReactFlow nodes={nodes} edges={edges} />
       </div>
+    </div>
     </div>
   );
 }
